64927913ee177f7fb4a50dc758e30388f70197a9
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-11-10 05:21:03 +0600

    add dynamic circular progressBar

--- src/Pages/Home/Skills/SkillCard/SkillCard.js
+++ src/Pages/Home/Skills/SkillCard/SkillCard.js
@@ -1,20 +1,54 @@
 import React from 'react';
 
-const SkillCard = ({name, value}) => {
+const SkillCard = ({ name, value }) => {
 
+    window.onload = () => {
+        const circularProgress = document.querySelectorAll('.circular-progress');
+        const progressValue = document.querySelectorAll('.progress-value');
+
+        circularProgress.forEach((circle, index) => {
+            let startValue = 0,
+                speed = 30
+
+                let endValue ;
+                if(index === 0){
+                    endValue = 90
+                }
+                if(index === 1){
+                    endValue = 85
+                }
+                if(index === 2){
+                    endValue = 100
+                }
+                if(index === 3){
+                    endValue = 95
+                }
+
+            const progress = setInterval(() => {
+                startValue++
+                
+                progressValue[index].innerHTML = `${startValue}%`;
+                circle.style.background = `conic-gradient(#72e2ae ${startValue * 3.6}deg, #ededed 0deg)`;
+
+                if (startValue === endValue) {
+                    clearInterval(progress)
+                }
+            }, speed)
+        })
+    }
     return (
         <div className='card w-[291px] h-[205px] text-center'>
             <div className='mb-[15px]'>
                 <div className="container">
-                    <div className='circular-progress' style={{ background: `conic-gradient(#72e2ae  ${value * 3.6}deg, #ededed 0deg)`}}>
+                    <div className='circular-progress' style={{ background: `conic-gradient(#72e2ae  ${value * 3.6}deg, #ededed 0deg)` }}>
                         <span className='progress-value text-white'>
-                            {value}%
+                            {value}
                         </span>
                     </div>
                 </div>
             </div>
             <h5 className='text-[#fafafa] text-[18px] leading-[1.5] font-medium'>
-               {name}
+                {name}
             </h5>
         </div>
     );
